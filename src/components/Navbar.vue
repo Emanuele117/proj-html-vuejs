e9d4b08c65b4820713d40ca78bb9120840e5f213
Emanuele117 <emanuele.pica7@gmail.com>
2021-11-25 21:35:37 +0100

    importazione img

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="contenitore-navbar">
+    <a href="home" class="home">{{ home }}</a>
+    <a href="history">{{ history }}</a>
+    <a href="team">{{ team }}</a>
+    <a href="menu">{{ menu }}</a>
+    <a href="takeout">{{ takeout }}</a>
+    <a href="bulletin">{{ bulletin }}</a>
+    <a href="reservations">{{ reservation }}</a>
+    <a href=""></a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: String,
+    history: String,
+    team: String,
+    menu: String,
+    takeout: String,
+    bulletin: String,
+    reservation: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.contenitore-navbar {
+  position: absolute;
+  top: 0;
+  margin-top: 3rem;
+  margin-left: 6rem;
+  a {
+    text-decoration: none;
+    color: white;
+    opacity: 70%;
+    margin-right: 2.5rem;
+  }
+  .home {
+    color: white;
+    opacity: 100%;
+  }
+}
+</style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="contenitore-generale">
+    <div class="logo">
+      <img
+        src="../assets/logo-restaurant.png"
+        alt="logo-avada"
+        class="logo-avada"
+      />
+      <div class="contenitore-linea">
+        <span class="linea"></span>
+        <p class="p-linea">THE BEST TABLE IN TOWN</p>
+      </div>
+      <div class="menu">
+        <h1 class="titolo-menu">FINE DINING EXPERIENCE</h1>
+        <p class="p-menu">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe minus
+          sequi hic voluptatum
+        </p>
+        <span class="bottone">EXPLORE THE MENU</span>
+      </div>
+    </div>
+    <div class="hero-bar">
+      <img src="../assets/slider52x.jpg" alt="" class="img-bar" />
+      <Navbar
+        v-for="item in items"
+        :key="item.id"
+        :home="item.home"
+        :history="item.history"
+        :team="item.team"
+        :menu="item.menu"
+        :takeout="item.takeout"
+        :bulletin="item.bulletin"
+        :reservation="item.reservations"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar.vue";
+export default {
+  data() {
+    return {
+      items: [
+        {
+          home: "Home",
+          history: "Culinary History",
+          team: "Our Team",
+          menu: "Our Menu",
+          takeout: "Takeout",
+          bulletin: "Bulletin",
+          reservations: "Reservations",
+        },
+      ],
+    };
+  },
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss">
+.contenitore-generale {
+  display: flex;
+  height: 600px;
+  width: 100%;
+  .logo {
+    background-color: black;
+    width: 600px;
+    height: 100vh;
+    .logo-avada {
+      width: 250px;
+      margin-left: 4rem;
+      margin-top: 2rem;
+    }
+    .contenitore-linea {
+      display: flex;
+      margin-left: 4rem;
+      margin-top: 7rem;
+      .p-linea {
+        margin-left: 2rem;
+        color: white;
+        opacity: 70%;
+        font-size: 12px;
+      }
+      .linea {
+        width: 30px;
+        height: 1px;
+        background-color: grey;
+        margin-top: 8px;
+      }
+    }
+    .menu {
+      width: 280px;
+      margin-left: 122px;
+      margin-top: 2rem;
+      .titolo-menu {
+        font-weight: bold;
+        color: white;
+        font-size: 45px;
+        margin-bottom: 3rem;
+      }
+      .p-menu {
+        color: white;
+        opacity: 70%;
+        margin-bottom: 3rem;
+        font-size: 15px;
+      }
+      .bottone {
+        padding: 10px;
+        color: white;
+        font-size: 10px;
+        border: 1px solid white;
+      }
+    }
+  }
+  .img-bar {
+    width: 1000px;
+    height: 100vh;
+    position: relative;
+  }
+}
+</style>
