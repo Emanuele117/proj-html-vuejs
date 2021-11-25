9311436fc0353460a57b9756d55a7c9961ab8f38
Emanuele117 <emanuele.pica7@gmail.com>
2021-11-25 21:31:58 +0100

    creazione e importazione componenti

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <SiteHeader />
+    <SiteMain />
+    <SiteFooter />
+  </div>
 </template>
 
 <script>
+import SiteHeader from "./components/SiteHeader.vue";
+import SiteMain from "./components/SiteMain.vue";
+import SiteFooter from "./components/SiteFooter.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    SiteHeader,
+    SiteMain,
+    SiteFooter,
+  },
 };
 </script>
 
