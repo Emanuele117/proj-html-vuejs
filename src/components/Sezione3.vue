c88f1e2194fed9d667693c53dea9cb1f52e9069e
Emanuele117 <emanuele.pica7@gmail.com>
2021-11-26 13:58:31 +0100

    importazione fontawesome

---
+++ src/components/Sezione3.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container general">
+    <div class="contenitore-img">
+      <img src="../assets/blog72x.jpg" alt="" class="img-orange" />
+    </div>
+    <div class="contenitore-black">
+      <div class="contenitore-critiche">
+        <p class="p-critiche">MEGGY STEWART</p>
+        <h2 class="titolo-critiche">NEW YORK TIMES</h2>
+
+        <div class="icons">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+
+        <p class="p-testo">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem
+          aut sapiente veniam ea enim reiciendis.
+        </p>
+        <p class="p-testo">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit..
+        </p>
+        <span class="bottone-critiche">READ FULL ARTICLE</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.general {
+  display: flex;
+  justify-content: center;
+  .img-orange {
+    height: 600px;
+  }
+  .contenitore-black {
+    background-color: black;
+    height: 600px;
+    width: 550px;
+    .contenitore-critiche {
+      width: 400px;
+      padding: 50px;
+      margin-left: 4rem;
+      margin-top: 3rem;
+
+      .p-critiche {
+        color: white;
+        opacity: 70%;
+        font-size: 12px;
+      }
+      .titolo-critiche {
+        font-weight: bold;
+        color: white;
+        font-size: 30px;
+      }
+      .fa-star {
+        color: white;
+        font-size: 12px;
+        margin-top: 2rem;
+      }
+      .p-testo {
+        color: white;
+        opacity: 70%;
+        font-size: 12px;
+        margin-top: 2rem;
+        margin-bottom: 3rem;
+      }
+      .bottone-critiche {
+        padding: 12px;
+        color: white;
+        font-size: 10px;
+        border: 1px solid white;
+      }
+    }
+  }
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -2,19 +2,31 @@
   <div>
     <Sezione1 />
     <Sezione2 />
+
+    <p class="p-critics">What critics are saying about us</p>
+
+    <Sezione3 />
   </div>
 </template>
 
 <script>
 import Sezione1 from "./Sezione1.vue";
 import Sezione2 from "./Sezione2.vue";
+import Sezione3 from "./Sezione3.vue";
 export default {
   components: {
     Sezione1,
     Sezione2,
+    Sezione3,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+.p-critics {
+  text-align: center;
+  font-size: 35px;
+  margin-top: 4rem;
+  margin-bottom: -10rem;
+}
 </style>
