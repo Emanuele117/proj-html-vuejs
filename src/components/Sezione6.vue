a7e0fc94109023072c1519fe03ef96e8a48edceb
Emanuele117 <emanuele.pica7@gmail.com>
2021-11-28 18:41:29 +0100

    inserimento cards Sezione6

---
+++ src/components/Sezione6.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container d-flex">
+    <div class="card">
+      <img src="../assets/blog92x.jpg" alt="cappuccino" />
+      <h2>THE BEST COFFE IN TOWN</h2>
+      <p>By admin | January 7th, 2020 |</p>
+    </div>
+    <div class="card">
+      <img src="../assets/blog72x.jpg" alt="" />
+      <h2>DISCOVER OUR NEW MENU</h2>
+      <p></p>
+    </div>
+    <div class="card">
+      <img src="../assets/blog82x.jpg" alt="" />
+      <h2>WE KNOW ACCEPT SQUARE!</h2>
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+</style>
